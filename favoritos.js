ad8f4cfbc99582c04cf0ab2572f7b622aa9d9656
Tomás Pica <tomaspica31@gmail.com>
2022-08-01 22:48:53 -0300

    Proyecto

--- favoritos.js
+++ favoritos.js
@@ -1,29 +1,29 @@
 let favoritos;
 
-if(JSON.parse(localStorage.getItem('favoritos'))) {
-  favoritos = JSON.parse(localStorage.getItem('favoritos'))
+if (JSON.parse(localStorage.getItem("favoritos"))) {
+  favoritos = JSON.parse(localStorage.getItem("favoritos"));
 } else {
-  localStorage.setItem('favoritos', JSON.stringify([]))
-  favoritos = JSON.parse(localStorage.getItem('favoritos'))
+  localStorage.setItem("favoritos", JSON.stringify([]));
+  favoritos = JSON.parse(localStorage.getItem("favoritos"));
 }
 
-const body = document.getElementById('favContainer');
+const body = document.getElementById("favContainer");
 if (favoritos.length == 0) {
-    const texto = `
+  const texto = `
     <div class='favContainer'>
         <h1 class='txtFavoritos'>No hay favoritos</h1>
         <a class='btnVolver' href='index.html'>
             <button>VOLVER</button>
         </a>
-    </div>`;        
-    body.innerHTML += texto;    
+    </div>`;
+  body.innerHTML += texto;
 } else {
-    const titulo = `
+  const titulo = `
     <div class'favContainer'>
         <h1 class='txtFavoritos'>Favoritos</h1>
     </div>`;
-    body.innerHTML += titulo;
-    const table = `    
+  body.innerHTML += titulo;
+  const table = `    
     <div class="tableContainer">
     <table>
         <thead>
@@ -45,27 +45,42 @@ if (favoritos.length == 0) {
         <button>VOLVER</button>
     </a>
     </div`;
-    body.innerHTML += table
-    const tbody = document.getElementById('tbody')
-    for (let i = 0; i < favoritos.length; i++) {
-        const element = favoritos [i];
-        const { id, auto, precio, img, cantidad } = element;
-        const fav = `
+  body.innerHTML += table;
+  const tbody = document.getElementById("tbody");
+  for (let i = 0; i < favoritos.length; i++) {
+    const element = favoritos[i];
+    const { id, auto, precio, img, cantidad } = element;
+    const fav = `
         <tr id=${id}>
-            <th><img class="trash" src="./img/basura.png" height="50px" alt="basura" srcset=""></th>
+            <th><img id="${id}" class="trash" src="./img/basura.png" height="50px" alt="basura" srcset=""></th>
             <th class="detallesTabla"><img class="imgAutoFav" height="200px" src="${img}" alt="auto"><span class="nombreauto">${auto}</span></th>
             <th>${cantidad}</th>
             <th>$${(cantidad * precio).toLocaleString()}</th>
         </tr>
-        `
-        tbody.innerHTML += fav;
-        }
-
-        let BorrarFavoritos = document.getElementById("btnBorrar");
-        BorrarFavoritos.onclick = () => {
-        favoritos = []
-        localStorage.setItem('favoritos', JSON.stringify(favoritos))
-        location.reload();
-    }
-         
+        `;
+    tbody.innerHTML += fav;
+  }
 }
+
+const btnBorrar = document.getElementsByClassName("trash");
+
+for (let i = 0; i < btnBorrar.length; i++) {
+    const element = btnBorrar[i];
+    element.addEventListener("click", borrarAuto);
+}
+
+function borrarAuto(e) {
+    const btn = e.target;
+    const botonId = btn.getAttribute("id");
+    const autoEnFavoritos = favoritos.find(prod => prod.id == botonId);
+    const indice = favoritos.indexOf(autoEnFavoritos);
+    favoritos.splice(indice, 1);
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    location.reload();
+};
+
+let BorrarFavoritos = document.getElementById("btnBorrar");
+BorrarFavoritos.onclick = () => {
+    localStorage.clear('favoritos')
+    location.reload();
+};
--- index.js
+++ index.js
@@ -1,117 +1,78 @@
-let autos = [
-    {
-      "id":"001",
-      "auto":"Honda Civic",
-      "precio":"50000",
-      "img":"./img/hondacivic.png",
-      "desc":""
-    },
-    {
-      "id":"002",
-      "auto":"Ford Mondeo",
-      "precio":"90000",
-      "img":"./img/fordmondeo.png",
-      "desc":""
-    },
-    {
-      "id":"003",
-      "auto":"Chevrolet Cruze",
-      "precio":"70000",
-      "img":"./img/cruze.png",
-      "desc":""
-    },
-  ];
-  
-  let favoritos;
-  
-   if(JSON.parse(localStorage.getItem('favoritos'))) {
-    favoritos = JSON.parse(localStorage.getItem('favoritos'))
+let autos;
+
+let favoritos;
+
+if (JSON.parse(localStorage.getItem("favoritos"))) {
+  favoritos = JSON.parse(localStorage.getItem("favoritos"));
+} else {
+  localStorage.setItem("favoritos", JSON.stringify([]));
+  favoritos = JSON.parse(localStorage.getItem("favoritos"));
+}
+
+function fetchJSON() {
+  fetch("autos.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then((data) => {
+      autos = data;
+      let card = "";
+      data.forEach(function (auto) {
+        card += `
+            <article class="card">
+                <div>
+                    <img class="imgAuto" src=${auto.img} alt="${auto.auto}"/>
+                <div/>
+                <div>
+                    <p>${auto.auto}</p>
+                <div/>
+                <div>
+                    <p>$${auto.precio.toLocaleString()}</p>
+                <div/>
+                <div class="btn-container">
+                    <button id=${auto.id} class="btn btn-outline-primary btnAgregar">AGREGAR A FAVORITOS</button>
+                </div>
+            </article>
+              `;
+      });
+      const container = document.getElementById("container");
+      container.innerHTML = card;
+    })
+    .catch(function () {
+      document.write("Error");
+    });
+}
+fetchJSON();
+
+
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.matches("button.btn")) {
+    agregarAFavoritos(e.target.id);
+  }
+});
+
+function agregarAFavoritos(e) {
+  const autoEncontrado = autos.find((aut) => aut.id == e);
+  const enFavoritos = favoritos.find((aut) => aut.id == autoEncontrado.id);
+  console.log(enFavoritos);
+  if (!enFavoritos) {
+    favoritos.push({ ...autoEncontrado, cantidad: 1 });
   } else {
-    localStorage.setItem('favoritos', JSON.stringify([]))
-    favoritos = JSON.parse(localStorage.getItem('favoritos'))
+    let favoritosFiltrado = favoritos.filter((aut) => aut.id != enFavoritos.id);
+    favoritos = [
+      ...favoritosFiltrado,
+      { ...enFavoritos, cantidad: enFavoritos.cantidad + 1 },
+    ];
   }
-   
-   function desplegarAutos() {
-  
-    for (let i = 0; i < autos.length; i++) {
-      const element = autos[i];
-      const { id, auto, precio, img } = element
-      const card = `
-      <div class='card'>
-        <div>
-         <img class='imgAuto' src=${img} width='350px' alt=''/>
-        </div>
-        <p>${auto}</p>
-        <div>
-          <p>$${precio.toLocaleString()}</p>
-        </div>
-        <div class="btn-container">
-          <button id=${id} class='btnAgregar'>AGREGAR A FAVORITOS</button>
-        </div>
-      </div>
-      `
-      const container = document.getElementById('container')
-      container.innerHTML += card
-    }
-  }
-  
-  desplegarAutos()
-  
-  const btnAgregar = document.getElementsByClassName('btnAgregar')
-  
-  for (let i = 0; i < btnAgregar.length; i++) {
-    const element = btnAgregar[i];
-    element.addEventListener('click', agregarAFavoritos)
-    
-  }
-  
-  function agregarAFavoritos(e) {
-    const btn = e.target;
-    const idBoton = btn.getAttribute('id')
-    const autoEncontrado = autos.find(aut => aut.id == idBoton)
-    const enFavoritos = favoritos.find(aut => aut.id == autoEncontrado.id)
-    console.log(enFavoritos)
-    if(!enFavoritos) {
-      favoritos.push({...autoEncontrado, cantidad: 1})
-    } else {
-      let favoritosFiltrado = favoritos.filter(aut => aut.id != enFavoritos.id)
-      favoritos = [...favoritosFiltrado, {...enFavoritos, cantidad: enFavoritos.cantidad + 1}]
-    }
-    console.log(favoritos)
-    localStorage.setItem('favoritos', JSON.stringify(favoritos))
-   
-    Toastify({
-      text:"Agregaste este producto a favoritos",
-      duration:1500,
-      onClick: function(){}
-    }).showToast();
+  console.log(favoritos);
+  localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+  Toastify({
+    text: "Agregaste este producto a favoritos",
+    duration: 1500,
+    onClick: function () {},
+  }).showToast();
 }
-  
-  const contador = document.getElementById('favCounter')
-  contador.innerHTML = favoritos.length
-  
-  /* const createCard = (p) => {
-    contador.innerHTML += `
-      <div>
-        <h2>${p.species.name}</h2>
-        <img src="${p.sprites.front_default}" alt="">
-      </div>  
-    `
-  } */
 
-
-/* fetch('./autos.json')
-  .then(response => console.log(response))
-   .then(data => {
-      console.log(data);
-      createCard(data)
-    }) 
-    .catch(error => console.error(error)) */
-
-const fetchLocalData = () => {
-  fetch('./autos.json').then((response) => response.json())
-  .then((result) => {
-    console.log(result)
-  })
-}
-fetchLocalData();
+const contador = document.getElementById("favCounter");
+contador.innerHTML = favoritos.length;
